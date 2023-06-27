2a9f2630ce5343833763cacdd6ba98a43c347ace
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2023-06-27 11:15:28 +0200

    Released 0.1.2.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
+# 0.1.2
+- export styles.dart
 # 0.1.1
 - export patterns.dart
 # 0.1.0
--- lib/nixy_markdown_controller.dart
+++ lib/nixy_markdown_controller.dart
@@ -3,3 +3,4 @@ library nixy_markdown_controller;
 
 export 'controller.dart';
 export 'patterns.dart';
+export 'styles.dart';
